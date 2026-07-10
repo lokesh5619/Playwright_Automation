ff040748d5c560a53d62cfde2773d516033bf700
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-07-10 21:23:19 +0700

    newChanges

--- tests/Demo/FacebookLogin.spec.ts
+++ tests/Demo/FacebookLogin.spec.ts
@@ -3,18 +3,23 @@ import {test,expect} from '@playwright/test'
 test('facebookLogin',async({page}) => {
     await page.goto('http://facebook.com');
 
+    //Facebook Login
     await page.locator("//input[@name='email' or @type='email']").fill('Lokesh vishwakarma');
 
     await page.locator("//input[@name='pass' or @type='password']").fill('626365');
 
+    //Create new account
     await page.locator("//span[text()='Create new account']").click();
 
     await page.locator("//span[text()='Get started on Facebook']").isVisible();
 
+    //First name
     await page.locator("//label[text()='First name']//preceding-sibling::input").fill('Lokesh');
 
+    //Surname
     await page.locator("//label[text()='Surname']//preceding-sibling::input").fill('Vishawakarma');
 
+    //Date of birth
     await page.locator("//span[text()='Day']/ancestor::div[@role='combobox']").click(); 
 
     await page.locator("//div[text()='5']").click();
@@ -27,12 +32,15 @@ test('facebookLogin',async({page}) => {
 
     await page.locator("//div[text()='2004']").click();
 
+    //Select Gender
     await page.locator("//span[text()='Select your gender']/ancestor::div[@role='combobox']").click();
 
     await page.locator("//div[text()='Male']").click();
 
+    //Enter mobile name
     await page.locator("//label[text()='Mobile number or email address']//preceding-sibling::input").fill('6263659258');
     
+    //Enter Password
     await page.locator("//label[text()='Password']//preceding-sibling::input").fill('lokesh@123');
 
     
