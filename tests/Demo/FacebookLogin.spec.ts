f8a173b46806c46690db69baf68f08974a0297f8
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-07-10 18:10:00 +0700

    newChanges

--- tests/Demo/FacebookLogin.spec.ts
+++ tests/Demo/FacebookLogin.spec.ts
@@ -3,9 +3,9 @@ import {test,expect} from '@playwright/test'
 test('facebookLogin',async({page}) => {
     await page.goto('http://facebook.com');
 
-    await page.locator("//input[@id='_R_1h6kqsqppb6amH1_' and @name='email']").fill('Lokesh vishwakarma');
+    await page.locator("//input[@name='email' or @type='email']").fill('Lokesh vishwakarma');
 
-    await page.locator("//input[@name='pass' and @type='password']").fill('626365');
+    await page.locator("//input[@name='pass' or @type='password']").fill('626365');
 
     await page.locator("//span[text()='Create new account']").click();
 
@@ -14,7 +14,22 @@ test('facebookLogin',async({page}) => {
     await page.locator("//label[text()='First name']//preceding-sibling::input").fill('Lokesh');
 
     await page.locator("//label[text()='Surname']//preceding-sibling::input").fill('Vishawakarma');
+
+    // await page.locator("//div[@role='combobox'][child::span[text()='Day']]").dblclick();   // nhi chala X
     
-    await expect(page).toHaveURL("/facebook/");
+    // await page.locator("//span[text()='Day']/parent::div").click();   // chal gya
+
+    await page.locator("//span[text()='Day']/ancestor::div[@role='combobox']").click();  // chal gya
+    
+    await page.locator("//label[text()='Mobile number or email address']//preceding-sibling::input").fill('6263659258');
+    
+    await page.locator("//label[text()='Mobile number or email address']//preceding-sibling::input").fill('');
+
+
+    // await page.locator("//div[text()='3']/ancestor::div[@role='listbox']").click();  // chal gya
+    // await page.locator("//*[@role='listbox']/child::div[2]").click();
+
+    // await expect(page).toHaveURL("/facebook/");
+
 })
 
