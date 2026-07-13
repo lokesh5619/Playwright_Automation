4b5d98ce771e9a15c888fa1bbf9a9b646356d508
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-07-13 18:26:39 +0700

    newChanges

--- tests/Demo/Amazon.spec.ts
+++ tests/Demo/Amazon.spec.ts
@@ -2,28 +2,35 @@ import {test , expect} from '@playwright/test'
 
 test('Amazon Login',async ({page}) => {
     await page.goto("https://Amazon.com");
+    await page.waitForTimeout(3000);
 
     //Search product
     await page.locator("//input[@type='text' or @placeholder='Search Amazon.in']").fill("watch");
+    await page.waitForTimeout(3000);
     
     await page.locator("//div[@aria-rowindex='1']").click();
+    await page.waitForTimeout(3000);
     
     //Select product
     await page.locator("(//span[@data-component-type='s-product-image'])[1]").click();
-
+    await page.waitForTimeout(3000);
+    
     // await page.locator("//div[@id='dp']//following::span[@id='wishListMainButton']").click();
     
     // await page.locator("//span[text()=' In stock ']//following::span[3]").click();
-
+    
     //Add Wish List 
     await page.locator("//span[@id='wishListMainButton']").click();
-
+    await page.waitForTimeout(3000);
+    
     await page.locator("//input[@type='email' or @name='email']").fill("6263659258");
-
-        // await page.locator("//span[text()='Continue']").click();
-
+    await page.waitForTimeout(3000);
+    
+    // await page.locator("//span[text()='Continue']").click();
+    
     await page.locator("//input[@type='email']//following::span[2]").click();
-
+    await page.waitForTimeout(3000);
+    
     // await page.waitForLoadState('networkidle');
     // await page.waitForLoadState('networkidle');
     
--- tests/Demo/orangeHRM2.spec.ts
+++ tests/Demo/orangeHRM2.spec.ts
@@ -1,6 +1,46 @@
 import {test, expect} from '@playwright/test'
 
 test("ORNGHRM",async({page})=>{
-     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',{timeout:20000});
+    await page.goto('http://facebook.com');
+    await page.waitForTimeout(1000);
     
+    await page.locator("[id=login_form]>div>div>div>div>div>div>div>input[name=email]").fill('Lokesh');
+    await page.waitForTimeout(1000);
+    
+    await page.locator("[id=login_form]>div>div>div>div:nth-child(2)>div>div>div>input").fill('lokesh');
+    await page.waitForTimeout(1000);
+    
+    await page.locator("[id=login_form]>div>div>div>div:nth-child(5)>div>a").click();
+    await page.waitForTimeout(1000);
+    
+    await page.locator("#_R_1cl2p4jikacppb6amH1_").fill("Lokesh");
+    await page.waitForTimeout(1000);
+    
+    await page.locator("#_R_1kl2p4jikacppb6amH1_").fill("Vishwakarma");
+    await page.waitForTimeout(1000);
+    
+    //Date of birth
+    //Day
+    await page.locator("[role=group]>div:first-child>div>div").click();
+    await page.waitForTimeout(1000);
+    
+    await page.locator("[role='group']>div:first-child>div>div>div:last-child[role='listbox']>div:nth-child(1)").click();
+    await page.waitForTimeout(1000);
+    
+    //Month
+    await page.locator("[role=group]>div:nth-child(2)>div>div").click();
+    await page.waitForTimeout(1000);
+    
+    await page.locator("[role='group']>div:nth-child(2)>div>div>div:last-child>div:nth-child(3)").click();
+    await page.waitForTimeout(1000);
+    
+    // //Year
+    await page.locator("[role=group]>div:last-child>div>div").click();
+    await page.waitForTimeout(1000);
+   
+
+    // await page.locator("[role=group]>div:last-child>div>div>div:last-child>div:nth-child(23)").click();
+    // await page.waitForTimeout(1000);
+
+// [role='group']>div:nth-child(2)>div>div>div:last-child[role='listbox']
 })
