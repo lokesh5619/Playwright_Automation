063b7dde28b145fcf96a362f8ee063da7a0eb927
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-07-17 16:03:13 +0700

    newChanges and newFile

--- Pages/LeftPanel.ts
+++ Pages/LeftPanel.ts
@@ -18,61 +18,67 @@ export class LeftPanel
     async clickOnPIM()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(2)");
         await adminClick.click();
     }
     async clickOnLeave()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(3)");
         await adminClick.click();
     }
     async clickOnTime()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(4)");
         await adminClick.click();
     }
     async clickOnRecruitment()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(5)");
         await adminClick.click();
     }
     async clickOnMyInfo()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(6)");
         await adminClick.click();
     }
     async clickOnMyPerformance()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(7)");
         await adminClick.click();
     }
     async clickOnDashboard()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(8)");
         await adminClick.click();
     }
     async clickOnDirectory()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(9)");
         await adminClick.click();
     }
     async clickMaintenance()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(10)");
         await adminClick.click();
     }
     async clickOnClaim()
     {
         console.log("Click on Admin tab in left panel.");
-        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(1)");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(11)");
+        await adminClick.click();
+    }
+    async clickOnBuzz()
+    {
+        console.log("Click on Admin tab in left panel.");
+        const adminClick = this.page.locator("[role='navigation']>div:last-child>:last-child>:nth-child(12)");
         await adminClick.click();
     }
 }
